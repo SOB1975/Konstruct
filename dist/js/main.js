0d5c6376c811023b81ac84ad122d1d2b7555d070
Oleg <stroleg912@gmail.com>
2022-09-02 12:34:36 +0300

    add file

---
+++ dist/js/main.js
@@ -0,0 +1,5 @@
+(()=>{"use strict";var __webpack_modules__={"./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/(__unused_webpack___webpack_module__,__webpack_exports__,__webpack_require__)=>{eval("__webpack_require__.r(__webpack_exports__);\n\n\n//# sourceURL=webpack://sob/./src/js/main.js?")}},__webpack_require__={r:_=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(_,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(_,"__esModule",{value:!0})}},__webpack_exports__={};__webpack_modules__["./src/js/main.js"](0,__webpack_exports__,__webpack_require__)})();
+//# sourceMappingURL=main.js.map
